1621d1e06cc829567ac9d5e6eccf21748cc9f154
Adrienne <adrienneeaston@gmail.com>
2020-09-01 19:17:36 -0700

    some styling

--- src/index.js
+++ src/index.js
@@ -7,40 +7,28 @@ class Quote extends React.Component {
     super(props);
     this.state = {
       quote: '"If you’re always trying to be normal you will never know how amazing you can be."',
-      author: '--Maya Angelou'
-      // randomIndex: '5'
+      author: '--Maya Angelou',
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick() {
     const randomQuotes = [
-      ['"To be or not to be, that is the question."', 'William Shakespeare'],
-      ['"The best revenge is massive success."', 'Frank Sinatra'],
-      ['"The human race has one really effective weapon and that is laughter."', 'Mark Twain'],
-      ['"Spoon!"', 'The Tick'],
+      ['"To be or not to be, that is the question."', '--William Shakespeare'],
+      ['"The best revenge is massive success."', '--Frank Sinatra'],
+      ['"The human race has one really effective weapon and that is laughter."', '--Mark Twain'],
+      ['"Spoon!"', '--The Tick'],
       ['"If you’re always trying to be normal you will never know how amazing you can be."', '--Maya Angelou'],
-      ['"Don\'t believe everything you hear on the internet."', 'Abraham Lincoln']
+      ['"Don\'t believe everything you see on the internet."', '--Abraham Lincoln']
     ]
     let randomIndex = Math.floor(Math.random() * 6)
     this.setState({
       quote: randomQuotes[randomIndex][0],
-      author: randomQuotes[randomIndex][1]
-      // randomIndex: Math.floor(Math.random() * 5)
+      author: randomQuotes[randomIndex][1],
     });
   }
 
   render() {
-    // const randomQuotes = [
-    //   ['"To be or not to be, that is the question."', 'William Shakespeare'],
-    //   ['"The best revenge is massive success."', 'Frank Sinatra'],
-    //   ['"The human race has one really effective weapon and that is laughter."', 'Mark Twain'],
-    //   ['"Spoon!"', 'The Tick'],
-    //   ['"If you’re always trying to be normal you will never know how amazing you can be."', '--Maya Angelou']
-    // ]
-    // console.log('index', randomIndex);
-    // let answer = randomQuotes[this.state.randomIndex];
-    // console.log('type', answer);
     return (
       <div id="quote-box">
           <p id="text">{this.state.quote}</p>
