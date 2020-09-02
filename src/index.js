9a72a91e03569ba5827641526cd4848a6444226d
Adrienne <adrienneeaston@gmail.com>
2020-09-01 17:10:55 -0700

    random quote and author rendering with click

--- src/index.js
+++ src/index.js
@@ -6,20 +6,40 @@ class Quote extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      quote: '"Hello Jay!"',
-      author: '--Adrienne'
+      quote: '"If you’re always trying to be normal you will never know how amazing you can be."',
+      author: '--Maya Angelou'
+      // randomIndex: '5'
     }
     this.handleClick = this.handleClick.bind(this);
   }
 
-  handleClick(event) {
+  handleClick() {
+    const randomQuotes = [
+      ['"To be or not to be, that is the question."', 'William Shakespeare'],
+      ['"The best revenge is massive success."', 'Frank Sinatra'],
+      ['"The human race has one really effective weapon and that is laughter."', 'Mark Twain'],
+      ['"Spoon!"', 'The Tick'],
+      ['"If you’re always trying to be normal you will never know how amazing you can be."', '--Maya Angelou']
+    ]
+    let randomIndex = Math.floor(Math.random() * 5)
     this.setState({
-      quote: '"Hello Adrienne!"',
-      author: '--Jay'
+      quote: randomQuotes[randomIndex][0],
+      author: randomQuotes[randomIndex][1]
+      // randomIndex: Math.floor(Math.random() * 5)
     });
   }
 
   render() {
+    // const randomQuotes = [
+    //   ['"To be or not to be, that is the question."', 'William Shakespeare'],
+    //   ['"The best revenge is massive success."', 'Frank Sinatra'],
+    //   ['"The human race has one really effective weapon and that is laughter."', 'Mark Twain'],
+    //   ['"Spoon!"', 'The Tick'],
+    //   ['"If you’re always trying to be normal you will never know how amazing you can be."', '--Maya Angelou']
+    // ]
+    // console.log('index', randomIndex);
+    // let answer = randomQuotes[this.state.randomIndex];
+    // console.log('type', answer);
     return (
       <div className="quote-box">
           <p id="text">{this.state.quote}</p>
